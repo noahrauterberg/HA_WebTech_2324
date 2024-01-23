eb79c64138140d955f248924d2472d302726565e
noah <rauterberg@campus.tu-berlin.de>
2024-01-23 12:09:38 +0100

    added ha05

---
+++ ha05/src/components/Board.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import Column from './Column.vue';
+
+	defineProps({
+		columns: {
+            type: Array,
+			required: true
+		}
+	});
+</script>
+
+<template>
+	<div class="container">
+		<div class="row">
+			<div v-for="column in columns" class="col-12 col-md-6 col-lg-4">
+                <Column :key="column.id" :column="column" />
+			</div>
+		</div>
+	</div>
+</template>
